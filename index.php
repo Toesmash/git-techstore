737a1294dc1b216d52f1abe0b0a5c3323783d719
Toesmash <tommy.chvalny@gmail.com>
2015-12-20 19:11:03 +0100

    navbar
    Changed font, started to work on navbar

--- index.php
+++ index.php
@@ -1,11 +1,46 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset='utf-8'>
 	<title>TECHstore</title>
+	<meta name="description" content="TECHstore"> 
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+	<link rel="stylesheet" href="styles/style.css">
+
+
 </head>
 <body>
+	<!-- <div class="navbar-outer"> -->
+	<nav class="navbar navbar-default navbar-fixed-top navbar-fixed-width" id="my-navbar" role="navigation">
+  		<div class="container">
+  			<div class="navbar-header">
+  				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+  					<a href="#" class="btn btn-info btn-md">
+          <span class="glyphicon glyphicon-tasks"></span>
+        </a>
+  				</button>
+  			</div>
+  			<div class="collapse navbar-collapse" id="navbar-collapse">
+  				<ul class="nav navbar-nav">
+  					<li><a href="#domov">Domov</a> 
+  					<li><a href="#ponuka">Ponuka</a> 
+  					<li><a href="#mojucet">Moj ucet</a> 
+  					<li><a href="#signin">Prihlas sa</a> 
+  					<li><a href="#contact">Kontakt</a> 
+  				</ul>
+  			</div>
+  		</div>
+  	</nav>
+  <!-- </div> -->
 	
 
+	<script src="http://code.jquery.com/jquery-2.1.4.min.js"> </script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </body>
 
 </html>
