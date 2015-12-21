8a69734b2d546a07cf49b7c232a534df2b3deed7
Toesmash <tommy.chvalny@gmail.com>
2015-12-22 00:49:06 +0100

    carousel
    Started on carousel, still need to change transitions for same height

--- index.php
+++ index.php
@@ -44,58 +44,34 @@
         </div>
       </div>
     </div>
-    <div>
-      <div class="row">
-        <div class="col-xs-1">
-          <p>Loren Ipsum</p>
-        </div>
-        <div class="col-xs-1">
-          <p>Loren Ipsum</p>
-        </div>
-        <div class="col-xs-1">
-          <p>Loren Ipsum</p>
-        </div>
-        <div class="col-xs-1">
-          <p>Loren Ipsum</p>
-        </div>
-        <div class="col-xs-1">
-          <p>Loren Ipsum</p>
-        </div>
-        <div class="col-xs-1">
-          <p>Loren Ipsum</p>
-        </div>
-        <div class="col-xs-1">
-          <p>Loren Ipsum</p>
-        </div>
-        <div class="col-xs-1">
-          <p>Loren Ipsum</p>
-        </div>
-        <div class="col-xs-1">
-          <p>Loren Ipsum</p>
-        </div>
-        <div class="col-xs-1">
-          <p>Loren Ipsum</p>
-        </div>
-        <div class="col-xs-1">
-          <p>Loren Ipsum</p>
-        </div>
-        <div class="col-xs-1">
-          <p>Loren Ipsum</p>
-        </div>
-      </div>
-    </div>
     <div class="row">
-      <div class="col-md-3">
-        <p>Lorem Ipsum je fiktívny text, používaný pri návrhu tlačovín a typografie. Lorem Ipsum je štandardným výplňovým textom už od 16. storočia, keď neznámy tlačiar zobral sadzobnicu plnú tlačových znakov a pomiešal ich, aby tak vytvoril vzorkovú knihu. Prežil nielen päť storočí, ale aj skok do elektronickej sadzby, a pritom zostal v podstate nezmenený. Spopularizovaný bol v 60-tych rokoch 20.storočia, vydaním hárkov Letraset, ktoré obsahovali pasáže Lorem Ipsum, a neskôr aj publikačným softvérom ako Aldus PageMaker, ktorý obsahoval verzie Lorem Ipsum.
+      <div  class="col-md-8">
+        <div class="carousel slide" id="my-carousel">
 
-Je dávno známe, že ak je zrozumiteľný obsah stránky, na ktorej rozloženie sa čitateľ díva, jeho pozornosť je rozptýlená. Dôvodom použitia Lorem Ipsum je fakt, že má viacmenej normálne rozloženie písmen, takže oproti použitiu 'Sem príde text, sem príde text' sa obsah vypĺňanej oblasti na stránke viac podobá na skutočný text. Mnohé balíky publikačného softvéru a editorov webových stránok už používajú Lorem Ipsum ako predvolený výplňový text a keď dáte na internete vyhľadávať 'lorem ipsum', objavíte mnoho webových stránok v rannom štádiu ich vzniku. V minulých rokoch sa objavilo mnoho verzií tohto textu, niekedy náhodou, niekedy úmyselne (pridaný humor a podobne).
+          <ol class="carousel-indicators">
+            <li data-target="#my-carousel" data-slide-to = "0" class="active"></li>
+            <li data-target="#my-carousel" data-slide-to = "1"></li>
+            <li data-target="#my-carousel" data-slide-to = "2"></li>
+          </ol>
 
- 
-Napriek všeobecnému presvedčeniu nie je Lorem Ipsum len náhodný text. Jeho korene sú v časti klasickej latinskej literatúry z roku 45 pred n.l., takže má viac ako 2000 rokov. Richard McClintock, profesor latinčiny na Hampden-Sydney College vo Virgínii, hľadal jedno z menej určitých latinských slov, consectetur, z pasáže Lorem Ipsum, a ako vyhľadával výskyt tohto slova v klasickej literatúre, objavil jeho nepochybný zdroj. Lorem Ipsum pochádza z odsekov 1.10.32 a 1.10.33 Cicerovho diela "De finibus bonorum et malorum" (O najvyššom dobre a zle), napísaného v roku 45 pred n.l. Táto kniha je pojednaním o teórii etiky, a bola veľmi populárna v renesancii. Prvý riadok Lorem Ipsum, "Lorem ipsum dolor sit amet..", je z riadku v odseku 1.10.32.
-
-Štandardný úsek Lorem Ipsum, používaný od 16. storočia, je pre zaujímavosť uvedený nižšie. Odseky 1.10.32 a 1.10.33 z "De finibus bonorum et malorum" od Cicera tu sú tiež uvedené v ich presnom pôvodnom tvare, doplnené anglickými verziami z roku 1914, v preklade H. Rackhama.
-
-Existuje mnoho podôb pasáží Lorem Ipsum, ale väčšina trpela rôznymi zmenami, vložením humoru, alebo náhodných slov, ktoré nevyzerajú ani trocha dôveryhodne. Ak sa chystáte použiť pasáž z Lorem Ipsum, mali by ste sa presvedčiť, že uprostred textu nie je skrytá žiadna časť, ktorá by vás mohla priviesť do nepríjemnej situácie. Všetky generátory Lorem Ipsum na internete opakujú vopred definované časti textu, takže náš generátor je prvým skutočným generátorom na internete. Používa slovník viac ako 200 latinských slov, a kombinuje ich niekoľkými modelovými vetnými štruktúrami, takže generuje Lorem Ipsum, ktoré vyzerá hodnoverne. Vygenerované Lorem Ipsum je týmto spôsobom vždy bez opakujúcich sa častí, bez vti</p>
+          <div class="carousel-inner">
+            <div class="item active">
+              <img src="img/carousel/apple-products.jpg" alt="Apple products" class="img-responsive">
+            </div>
+            <div class="item">
+              <img src="img/carousel/watch.jpg" alt="Apple watch" class="img-responsive">
+            </div>
+            <div class="item">
+              <img src="img/carousel/headphones.jpg" alt="Headphones" class="img-responsive">
+            </div>
+            <a href="#my-carousel" class="carousel-control left" data-slide="prev">
+              <span class="icon-prev"></span>
+            </a>
+            <a href="#my-carousel" class="carousel-control right" data-slide="next">
+              <span class="icon-next"></span>
+            </a>
+          </div>
+        </div>   
       </div>
     </div>
 
