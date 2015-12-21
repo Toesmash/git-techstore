bd573e5457dffdf0535c22a04f3fdcecb334f0d3
Toesmash <tommy.chvalny@gmail.com>
2015-12-21 22:28:48 +0100

    nav-done
    Navbar finished, starting to work on jumbotron and main content

--- index.php
+++ index.php
@@ -14,87 +14,96 @@
 
 </head>
 <body>
-	<!-- <div class="navbar-outer"> -->
-	<nav class="navbar navbar-default navbar-fixed-top navbar-fixed-width" id="my-navbar" role="navigation">
-    <div class="container">
-     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
-        <a href="#" class="btn btn-warning btn-md">
-        <span class="glyphicon glyphicon-tasks"></span>
-      </a>
-    </button>
-    <a class="navbar-brand" href="#">TECHstore</a>
-  </div>
-  <div class="collapse navbar-collapse" id="navbar-collapse">
-    <ul class="nav navbar-nav">
-     <li><a href="#domov">Domov</a> 
-       <li><a href="#ponuka">Ponuka</a> 
-         <li><a href="#mojucet">Moj ucet</a> 
-           <li><a href="#signin">Prihlas sa</a> 
-             <li><a href="#contact">Kontakt</a> 
-             </ul>
-           </div>
-         </div>
-       </nav>
-
-       <div class="jumbotron">
-        <div class="container text-left">
-          <div class="row">
-            <div class="col-lq-6">
-
-              <img src="img/logo.svg" class="img-logo">
-              <h1>TECHstore</h1>
-            </div>
-
+  <div class="container-fluid">
+    <nav class="navbar navbar-default navbar-fixed-top " id="my-navbar" role="navigation">
+      <div class="container-fluid">
+        <!-- <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+          <span class="glyphicon glyphicon-tasks"></span>
+        </button> -->
+        <div class="collapse navbar-collapse" id="navbar-collapse navbar-header">
+          <div class="navbar-header">
+            <a href="#domov"><img src="img/logo-primary2.svg" class="navbar-brand"></a>
           </div>
-
+          <ul class="nav navbar-nav">
+            <li><a href="#ponuka">Ponuka</a> 
+            <li><a href="#mojucet">Moj ucet</a> 
+            <li><a href="#signin">Prihlas sa</a> 
+            <li><a href="#contact">Kontakt</a> 
+          </ul>
         </div>
       </div>
+    </nav>
 
-      <div class="containter navbar-fixed-width">
-        <div class="row">
-          
-          <div class="col-xs-1">
-            <p>LOREM IPSUM DOLOR...</p>
-          </div>
-          <div class="col-sm-1">
-            <p>LOREM IPSUM DOLOR...</p>
-          </div>
-          <div class="col-md-1">
-            <p>LOREM IPSUM DOLOR...</p>
-          </div>
-          <div class="col-lq-1">
-            <p>LOREM IPSUM DOLOR...</p>
-          </div>
-          <div class="col-xs-1">
-            <p>LOREM IPSUM DOLOR...</p>
-          </div>
-          <div class="col-xs-1">
-            <p>LOREM IPSUM DOLOR...</p>
-          </div>
-          <div class="col-xs-1">
-            <p>LOREM IPSUM DOLOR...</p>
-          </div>
-          <div class="col-xs-1">
-            <p>LOREM IPSUM DOLOR...</p>
-          </div>
-          <div class="col-xs-1">
-            <p>LOREM IPSUM DOLOR...</p>
-          </div>
-          <div class="col-xs-1">
-            <p>LOREM IPSUM DOLOR...</p>
-          </div>
-          <div class="col-xs-1">
-            <p>LOREM IPSUM DOLOR...</p>
-          </div>
+    <div class="jumbotron">
+      <div class="row">
+        <div class="col-md-2">
+          <img src="img/logo.svg" class="img-logo">
+        </div>
+        <div class="col-md-4">
+            <img src="img/logo-primary2.svg">
         </div>
       </div>
-      <!-- </div> -->
+    </div>
+    <div>
+      <div class="row">
+        <div class="col-xs-1">
+          <p>Loren Ipsum</p>
+        </div>
+        <div class="col-xs-1">
+          <p>Loren Ipsum</p>
+        </div>
+        <div class="col-xs-1">
+          <p>Loren Ipsum</p>
+        </div>
+        <div class="col-xs-1">
+          <p>Loren Ipsum</p>
+        </div>
+        <div class="col-xs-1">
+          <p>Loren Ipsum</p>
+        </div>
+        <div class="col-xs-1">
+          <p>Loren Ipsum</p>
+        </div>
+        <div class="col-xs-1">
+          <p>Loren Ipsum</p>
+        </div>
+        <div class="col-xs-1">
+          <p>Loren Ipsum</p>
+        </div>
+        <div class="col-xs-1">
+          <p>Loren Ipsum</p>
+        </div>
+        <div class="col-xs-1">
+          <p>Loren Ipsum</p>
+        </div>
+        <div class="col-xs-1">
+          <p>Loren Ipsum</p>
+        </div>
+        <div class="col-xs-1">
+          <p>Loren Ipsum</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <p>Lorem Ipsum je fiktívny text, používaný pri návrhu tlačovín a typografie. Lorem Ipsum je štandardným výplňovým textom už od 16. storočia, keď neznámy tlačiar zobral sadzobnicu plnú tlačových znakov a pomiešal ich, aby tak vytvoril vzorkovú knihu. Prežil nielen päť storočí, ale aj skok do elektronickej sadzby, a pritom zostal v podstate nezmenený. Spopularizovaný bol v 60-tych rokoch 20.storočia, vydaním hárkov Letraset, ktoré obsahovali pasáže Lorem Ipsum, a neskôr aj publikačným softvérom ako Aldus PageMaker, ktorý obsahoval verzie Lorem Ipsum.
+
+Je dávno známe, že ak je zrozumiteľný obsah stránky, na ktorej rozloženie sa čitateľ díva, jeho pozornosť je rozptýlená. Dôvodom použitia Lorem Ipsum je fakt, že má viacmenej normálne rozloženie písmen, takže oproti použitiu 'Sem príde text, sem príde text' sa obsah vypĺňanej oblasti na stránke viac podobá na skutočný text. Mnohé balíky publikačného softvéru a editorov webových stránok už používajú Lorem Ipsum ako predvolený výplňový text a keď dáte na internete vyhľadávať 'lorem ipsum', objavíte mnoho webových stránok v rannom štádiu ich vzniku. V minulých rokoch sa objavilo mnoho verzií tohto textu, niekedy náhodou, niekedy úmyselne (pridaný humor a podobne).
+
+ 
+Napriek všeobecnému presvedčeniu nie je Lorem Ipsum len náhodný text. Jeho korene sú v časti klasickej latinskej literatúry z roku 45 pred n.l., takže má viac ako 2000 rokov. Richard McClintock, profesor latinčiny na Hampden-Sydney College vo Virgínii, hľadal jedno z menej určitých latinských slov, consectetur, z pasáže Lorem Ipsum, a ako vyhľadával výskyt tohto slova v klasickej literatúre, objavil jeho nepochybný zdroj. Lorem Ipsum pochádza z odsekov 1.10.32 a 1.10.33 Cicerovho diela "De finibus bonorum et malorum" (O najvyššom dobre a zle), napísaného v roku 45 pred n.l. Táto kniha je pojednaním o teórii etiky, a bola veľmi populárna v renesancii. Prvý riadok Lorem Ipsum, "Lorem ipsum dolor sit amet..", je z riadku v odseku 1.10.32.
+
+Štandardný úsek Lorem Ipsum, používaný od 16. storočia, je pre zaujímavosť uvedený nižšie. Odseky 1.10.32 a 1.10.33 z "De finibus bonorum et malorum" od Cicera tu sú tiež uvedené v ich presnom pôvodnom tvare, doplnené anglickými verziami z roku 1914, v preklade H. Rackhama.
+
+Existuje mnoho podôb pasáží Lorem Ipsum, ale väčšina trpela rôznymi zmenami, vložením humoru, alebo náhodných slov, ktoré nevyzerajú ani trocha dôveryhodne. Ak sa chystáte použiť pasáž z Lorem Ipsum, mali by ste sa presvedčiť, že uprostred textu nie je skrytá žiadna časť, ktorá by vás mohla priviesť do nepríjemnej situácie. Všetky generátory Lorem Ipsum na internete opakujú vopred definované časti textu, takže náš generátor je prvým skutočným generátorom na internete. Používa slovník viac ako 200 latinských slov, a kombinuje ich niekoľkými modelovými vetnými štruktúrami, takže generuje Lorem Ipsum, ktoré vyzerá hodnoverne. Vygenerované Lorem Ipsum je týmto spôsobom vždy bez opakujúcich sa častí, bez vti</p>
+      </div>
+    </div>
 
 
-      <script src="http://code.jquery.com/jquery-2.1.4.min.js"> </script>
-      <!-- Latest compiled and minified JavaScript -->
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    </body>
 
-    </html>
+   </div> <!--Container -->
+  <script src="http://code.jquery.com/jquery-2.1.4.min.js"> </script>
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+</html>
